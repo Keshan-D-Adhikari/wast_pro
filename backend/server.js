bc734f71c9353e8a19877d3b235dfc1531fa1d57
Keshan-D-Adhikari <adhikarikeshan@gmail.com>
2025-12-12 07:12:33 +0530

    Add all backend files

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,24 @@
 //const express = require('express');
  
 import express from"express";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 
 const app = express();
+const PORT=process.env.PORT || 5001;
 
-app.listen(5001, () => {
+connectDB(process.env.DATABASE_URL);
 
 
-    app.get('/', (req, res) => {
+app.get('/', (req, res) => {
         res.send('Hello World!');
     });
 
-        console.log('Server is running on port 5001');
+
+app.listen(5001, () => {
+
+console.log('Server is running on port  : ',PORT);
+
 });
